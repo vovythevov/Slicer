5dd195d32d23d070e468c1b071e3e75f3933774e
pieper <pieper@3bd1e089-480b-0410-8dfb-8563597acbee>
2012-08-31 16:15:46 +0000

    ENH: improved testing of mrb saving
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20900 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/SlicerMRBTest.py
+++ Applications/SlicerApp/Testing/Python/SlicerMRBTest.py
@@ -13,6 +13,21 @@ Run manually from within slicer by pasting an version of this with the correct p
 execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Python/SlicerMRBTest.py'); t = SlicerMRB(); t.setUp(); t.runTest()
 
   """
+
+  def __init__(self,useCase='small',uniqueDirectory=False):
+    self.useCase = useCase
+    self.uniqueDirectory = uniqueDirectory
+
+  def delayDisplay(self,message,msec=1000):
+    print(message)
+    self.info = qt.QDialog()
+    self.infoLayout = qt.QVBoxLayout()
+    self.info.setLayout(self.infoLayout)
+    self.label = qt.QLabel(message,self.info)
+    self.infoLayout.addWidget(self.label)
+    qt.QTimer.singleShot(msec, self.info.close)
+    self.info.exec_()
+
   def setUp(self):
     slicer.mrmlScene.Clear(0)
 
@@ -28,32 +43,48 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
 
     #
     # first, get the data
+    # - amount of data depends on useCase attribue
     #
     import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5766', 'DTIVolume.raw.gz', None),
-        ('http://slicer.kitware.com/midas3/download?items=5765', 'DTIVolume.nhdr', slicer.util.loadVolume),
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        ('http://slicer.kitware.com/midas3/download?items=5768', 'tract1.vtk', slicer.util.loadFiberBundle),
-        ('http://slicer.kitware.com/midas3/download?items=5769', 'tract2.vtk', slicer.util.loadFiberBundle),
-        ('http://slicer.kitware.com/midas3/download?items=5770', 'tract3.vtk', slicer.util.loadFiberBundle),
-        )
+    if self.useCase == 'small':
+      downloads = (
+          ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
+          ('http://slicer.kitware.com/midas3/download?items=5768', 'tract1.vtk', slicer.util.loadFiberBundle),
+          )
+      tracts = ('tract1',)
+      tractColors = ( (0.2, 0.9, 0.3), )
+    else:
+      downloads = (
+          ('http://slicer.kitware.com/midas3/download?items=5766', 'DTIVolume.raw.gz', None),
+          ('http://slicer.kitware.com/midas3/download?items=5765', 'DTIVolume.nhdr', slicer.util.loadVolume),
+          ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
+          ('http://slicer.kitware.com/midas3/download?items=5768', 'tract1.vtk', slicer.util.loadFiberBundle),
+          ('http://slicer.kitware.com/midas3/download?items=5769', 'tract2.vtk', slicer.util.loadFiberBundle),
+          ('http://slicer.kitware.com/midas3/download?items=5770', 'tract3.vtk', slicer.util.loadFiberBundle),
+          )
+      tracts = ('tract1', 'tract2', 'tract3',)
+      tractColors = ( (0.2, 0.9, 0.3), (0.9, 0.3, 0.3), (0.2, 0.4, 0.9) )
 
+    # perform the downloads if needed, then load
     for url,name,loader in downloads:
       filePath = slicer.app.temporaryPath + '/' + name
       if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        print('Requesting download %s from %s...\n' % (name, url))
+        self.delayDisplay('Requesting download %s from %s...\n' % (name, url))
         urllib.urlretrieve(url, filePath)
       if loader:
-        print('Loading %s...\n' % (name,))
+        self.delayDisplay('Loading %s...\n' % (name,))
         loader(filePath)
-    print('Finished with download and loading\n')
+    self.delayDisplay('Finished with download and loading\n')
+
+    # confirm that FA is in the background of the Red slice
+    redComposite = slicer.util.getNode('vtkMRMLSliceCompositeNodeRed')
+    fa = slicer.util.getNode('FA')
+    self.assertTrue( redComposite.GetBackgroundVolumeID() == fa.GetID() )
+    self.delayDisplay('The FA volume is in the background of the Red viewer')
 
     #
     # set up the view of the tracts and scene views
     #
-    tracts = ('tract1', 'tract2', 'tract3',)
-    tractColors = ( (0.2, 0.9, 0.3), (0.9, 0.3, 0.3), (0.2, 0.4, 0.9) )
     for tractName,tubeColor in zip(tracts, tractColors):
       fiberNode = slicer.util.getNode(tractName)
       tubeDisplay = fiberNode.GetTubeDisplayNode()
@@ -71,23 +102,39 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
     applicationLogic = slicer.app.applicationLogic()
     sceneSaveDirectory = self.tempDirectory('__scene__')
     mrbFilePath= self.tempDirectory('__mrb__') + '/test.mrb'
-    print("Saving scene to: %s" % sceneSaveDirectory)
-    print("Saving mrb to: %s" % mrbFilePath)
+    self.delayDisplay("Saving scene to: %s\n" % sceneSaveDirectory + "Saving mrb to: %s" % mrbFilePath)
     applicationLogic.SaveSceneToSlicerDataBundleDirectory(sceneSaveDirectory, None)
-    print("Finished saving scene")
+    self.delayDisplay("Finished saving scene")
     applicationLogic.Zip(mrbFilePath,sceneSaveDirectory)
-    print("Finished saving MRB")
+    self.delayDisplay("Finished saving MRB")
+
+    # confirm again that FA is in the background of the Red slice
+    self.delayDisplay('Check that FA volume is STILL in the background of the Red viewer')
+    redComposite = slicer.util.getNode('vtkMRMLSliceCompositeNodeRed')
+    fa = slicer.util.getNode('FA')
+    self.assertTrue( redComposite.GetBackgroundVolumeID() == fa.GetID() )
+    self.delayDisplay('The FA volume is STILL in the background of the Red viewer')
     
     #
     # save image, reload the scene and save image
     # then load mrb and save image
     # - all images should be the same
     #
+    self.delayDisplay('Save and image, then close scene')
     self.beforeImage = qt.QPixmap.grabWidget(slicer.util.mainWindow()).toImage()
     slicer.mrmlScene.Clear(0)
+    self.delayDisplay('Now, reload the saved scene')
     slicer.mrmlScene.SetURL(sceneSaveDirectory + '/' + os.path.basename(sceneSaveDirectory) + '.mrml')
     slicer.mrmlScene.Import()
     slicer.app.processEvents()
+
+    # confirm again that FA is in the background of the Red slice
+    self.delayDisplay('IS the FA volume is AGAIN in the background of the Red viewer?')
+    redComposite = slicer.util.getNode('vtkMRMLSliceCompositeNodeRed')
+    fa = slicer.util.getNode('FA')
+    self.assertTrue( redComposite.GetBackgroundVolumeID() == fa.GetID() )
+    self.delayDisplay('The FA volume is AGAIN in the background of the Red viewer')
+
     self.sceneLoadedImage = qt.QPixmap.grabWidget(slicer.util.mainWindow()).toImage()
     slicer.mrmlScene.Clear(0)
     mrbExtractPath = self.tempDirectory('__mrb_extract__')
@@ -98,16 +145,23 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
     sceneMismatch = self.beforeImage != self.sceneLoadedImage
     mrbMismatch = self.beforeImage != self.mrbLoadedImage
     if sceneMismatch:
-      print('Loaded scene image does not match original scene image')
+      self.delayDisplay('Loaded scene image does not match original scene image')
     if mrbMismatch:
-      print('Loaded mrb image does not match original scene image')
+      self.delayDisplay('Loaded mrb image does not match original scene image')
 
     if (sceneMismatch or mrbMismatch):
       self.imageCompare((self.beforeImage, self.sceneLoadedImage, self.mrbLoadedImage), "before, after scene load, and after mrb load")
+      self.delayDisplay('have a quick look...', msec=10000)
+      imagesAndNames = ((self.beforeImage,'before'), (self.sceneLoadedImage,'scene'), (self.mrbLoadedImage,'mrb'))
+      for i,name in imagesAndNames:
+        tmp = self.tempDirectory('')
+        filePath = tmp + '/' + name + '.png'
+        print("Saving image to %s" % filePath)
+        i.save(filePath)
 
     self.assertTrue(not sceneMismatch and not mrbMismatch)
 
-    print("Scene and MRB loaded and compared")
+    self.delayDisplay("Scene and MRB loaded and compared")
 
   def imageCompare(self,images,text='',prefWidth=1500): 
     """Show images in a window with a text message.
@@ -168,13 +222,15 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
   def tempDirectory(self,key='__SlicerTestTemp__'):
     """Come up with a unique directory name in the temp dir and make it and return it
     # TODO: switch to QTemporaryDir in Qt5.
-    # For now, create a named directory
+    # For now, create a named directory if uniqueDirectory attribute is true
     Note: this directory is not automatically cleaned up
     """
     tempDir = qt.QDir(slicer.app.temporaryPath)
-    tempDirName = key + qt.QDateTime().currentDateTime().toString("yyyy-MM-dd_hh+mm+ss.zzz")
-    packFileInfo = qt.QFileInfo(qt.QDir(tempDir), tempDirName)
-    dirPath = packFileInfo.absoluteFilePath()
+    tempDirName = key
+    if self.uniqueDirectory:
+      key += qt.QDateTime().currentDateTime().toString("yyyy-MM-dd_hh+mm+ss.zzz")
+    fileInfo = qt.QFileInfo(qt.QDir(tempDir), tempDirName)
+    dirPath = fileInfo.absoluteFilePath()
     qt.QDir().mkpath(dirPath)
     return dirPath
 
